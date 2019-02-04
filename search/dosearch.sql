78a43f29c8d58611ae4ee0b6f49c31345e024e71
niquola <niquola@gmail.com>
2019-02-04 11:59:11 +0300

    upate

--- search/dosearch.sql
+++ search/dosearch.sql
@@ -1,17 +1,51 @@
 ----
--- Arrows and functional indexes
+-- Arrow functions
+
+select
+  substring(resource#>>'{subject,id}' from 0 for 10),
+  resource#>>'{type,0,text}',
+  (resource#>>'{period,start}')::date,
+  (resource#>>'{period,end}')::date
+from encounter
+limit 10
+
 ----
--- \a
+
+drop index enc_subj_id_idx;
+
+----
+-- Arrows and functional indexes
+
+\timing
 
 select
   resource#>>'{type,0,text}',
   (resource#>>'{period,start}')::date,
   (resource#>>'{period,end}')::date
--- ,jsonb_pretty(resource)
 from encounter
 where
 resource#>>'{subject,id}' = '32fe33e0-296f-486a-856d-3053fd0db8ab'
-order by resource#>>'{period,start}'
+
+----
+
+drop index enc_subj_id_idx;
+
+----
+create index enc_subj_id_idx on encounter (
+  (resource#>>'{subject,id}')
+);
+
+analyze encounter;
+
+----
+explain (costs off)
+select
+  resource#>>'{type,0,text}',
+  (resource#>>'{period,start}')::date,
+  (resource#>>'{period,end}')::date
+from encounter
+where
+resource#>>'{subject,id}' = '32fe33e0-296f-486a-856d-3053fd0db8ab'
 
 ----
 
@@ -21,10 +55,8 @@ order by resource#>>'{period,start}'
 \timing
 explain analyze
 select
-  -- id,
   (resource#>>'{period,start}')::date,
   resource#>>'{type,0,text}'
-  -- ,jsonb_pretty(resource)
 from encounter
 where (resource#>>'{period,start}')::date
 between '2011-04-01'::date and '2011-04-10'::date
@@ -32,11 +64,8 @@ limit 30
 ----
 
 CREATE OR REPLACE FUNCTION imm_date(text) RETURNS date
-AS 'select $1::date;'
-LANGUAGE SQL
-IMMUTABLE
-RETURNS NULL ON NULL INPUT;
-
+  AS 'select $1::date;'
+LANGUAGE SQL IMMUTABLE RETURNS NULL ON NULL INPUT;
 
 create index enc_start_idx on encounter
 (
@@ -47,7 +76,7 @@ vacuum analyze encounter;
 
 ----
 \timing
-explain analyze
+explain (costs off)
 select
   (resource#>>'{period,start}')::date,
   resource#>>'{type,0,text}'
@@ -64,40 +93,47 @@ limit 30
 \a
 
 select
-resource#>>'{code,coding,0,code}',
-resource#>>'{code,coding,0,system}',
-resource#>>'{code,coding,0,display}'
--- jsonb_pretty(resource)
+  resource#>>'{code,coding,0,code}',
+  resource#>>'{code,coding,0,system}',
+  resource#>>'{code,coding,0,display}'
 from observation
 where
-resource#>>'{code,coding,0,code}' = '72166-2'
+  resource#>>'{code,coding,0,code}' = '72166-2'
+  or
+  resource#>>'{code,coding,1,code}' = '72166-2'
 
 limit 100
 
 ----
 
+-- inclusion operator
+
 -- @> operator
 select '{"a":1, "b":2}'::jsonb @> '{"b":3}'::jsonb;
 
 ----
-select '{"a":1, "b":2}'::jsonb <@ '{"b":2}'::jsonb;
+select '{"a":1, "b":2}'::jsonb @> '{"b":2}'::jsonb;
 
 ----
 -- matches part of document
-select '[{"system":"phone", "value": "123"}]'::jsonb @> '[{"system":"phone"}]'::jsonb;
+select
+   '[{"system":"phone", "value": "123"}]'::jsonb
+    @> '[{"system":"phone"}]'::jsonb;
 
 ----
 ---match nested objects and arrays
 
 select $JSON$
 [
-{"prop": {"system":"email", "value": "123"}},
-{"prop": {"system":"phone", "value": "123"}}
+  {"prop": {"system":"email", "value": "123"}},
+  {"prop": {"system":"phone", "value": "123"}}
 ]
 $JSON$::jsonb @> '[{"prop": {"system":"phone"}}]'::jsonb;
 
 
 ----
+
+-- has key
 
 select
 '{"a":1, "b":2}'::jsonb ? 'b' as b_in,
@@ -106,22 +142,26 @@ select
 
 ----
 
-select '{"name": "Nikolai", "address": "spb.ru"}'::jsonb ?& array['name', 'address'] 
+-- all of keys
+
+select
+  '{"name": "Nikolai", "address": "spb.ru"}'::jsonb
+   ?& array['name', 'address'] 
 ;
 
 ----
 
-select '{"name": "Nikolai", "address": "spb.ru"}'::jsonb ?& array['name', 'birthDate'] 
+select '{"name": "Nikolai", "address": "spb.ru"}'::jsonb
+   ?& array['name', 'birthDate']
 ;
 
 ----
 \timing
-explain analyze
+explain (costs off)
 select
-resource#>>'{code,coding,0,code}',
-resource#>>'{code,coding,0,system}',
-resource#>>'{code,coding,0,display}'
--- jsonb_pretty(resource)
+  resource#>>'{code,coding,0,code}',
+  resource#>>'{code,coding,0,system}',
+  resource#>>'{code,coding,0,display}'
 from observation
 where resource @> '{"code":{"coding":[{"code": "72166-2", "system": "http://loinc.org"}]}}'::jsonb
 
@@ -134,17 +174,40 @@ DROP index obs_jsq_idxgin;
 
 ----
 
-CREATE INDEX obs_idxginp ON observation USING GIN (resource jsonb_path_ops);
+-- default operator
+
+CREATE INDEX obs_idxgin
+   ON observation
+     USING GIN (resource);
+
+analyze observation;
 
 ----
 
-CREATE INDEX obs_idxgin ON observation USING GIN (resource);
+CREATE INDEX obs_idxginp
+ ON observation
+   USING GIN (resource jsonb_path_ops);
 
+analyze observation;
 ----
 
-select jsonb_pretty(table_size('observation'));
-select jsonb_pretty(table_size('obs_idxgin'));
-select jsonb_pretty(table_size('obs_idxginp'));
+select
+  table_size('observation')->'total' as table_size,
+  table_size('obs_idxgin')->'total' as jsonb_ops,
+  table_size('obs_idxginp')->'total' as jsonb_path_ops;
+
+----
+\timing
+explain (costs off)
+select
+  resource#>>'{code,coding,0,code}',
+  resource#>>'{code,coding,0,system}',
+  resource#>>'{code,coding,0,display}'
+from observation
+where resource @> '{"code":{"coding":[{"code": "72166-2", "system": "http://loinc.org"}]}}'::jsonb
+
+limit 100
+----
 
 ---
 -- jsquery
@@ -156,7 +219,7 @@ create extension if not exists jsquery;
 
 \timing
 
-explain analyze
+-- explain analyze
 select
   resource#>>'{subject,id}',
   resource#>>'{value,Quantity,value}',
@@ -172,9 +235,40 @@ limit 10;
 
 ----
 
-CREATE INDEX obs_jsq_idxgin ON observation USING GIN (resource jsonb_path_value_ops);
+--sepecil opclass
 
-VACUUM ANALYZE observation;
+CREATE INDEX obs_jsq_idxgin
+  ON observation
+    USING GIN (resource jsonb_path_value_ops);
+
+ANALYZE observation;
+
+----
+
+select
+  table_size('observation')->'total' as table_size,
+  table_size('obs_idxgin')->'total' as jsonb_ops,
+  table_size('obs_idxginp')->'total' as jsonb_path_ops,
+  table_size('obs_jsq_idxgin')->'total' as jsquery_ops
+
+----
+
+
+\timing
+
+explain (costs off)
+select
+  resource#>>'{subject,id}',
+  resource#>>'{value,Quantity,value}',
+  resource#>>'{value,Quantity,unit}'
+from observation
+where
+resource @@  $JSQ$
+code.coding.#.code = "2339-0"
+and subject.id /*-- index */ = "32fe33e0-296f-486a-856d-3053fd0db8ab"
+and value.Quantity.value > 80.0
+$JSQ$
+limit 10;
 
 ----
 
@@ -185,6 +279,8 @@ $JSQ$
   and value.Quantity.value > 80.0
 $JSQ$
 );
+
+----
 
 SELECT gin_debug_query_path_value(
 $JSQ$
@@ -230,16 +326,7 @@ limit 10;
 
 ----
 
-create extension fhirpath;
-
-SELECT fhirpath(resource,'name.given')
-from patient
-limit 10;
-
-----
-
 -- Analytics
-
 ---
 
 DROP materialized VIEW view_pts cascade;
@@ -272,10 +359,11 @@ where address ilike '%Spur%'
 limit 10
 
 ----
+insert into patient (id,txid,status, resource)
+select gen_random_uuid(),0, 'created', resource from patient;
+
+----
 
 REFRESH MATERIALIZED VIEW view_pts;
 
 ----
-insert into patient (id,txid,status, resource)
-select gen_random_uuid(),0, 'created', resource from patient;
-
